1f7836ea427385b2e46f62edd7fc2e243b6d831c
Namratha Prabhu <namrathaprabhu@Namrathas-MacBook-Pro.local>
2018-10-22 20:40:27 -0700

    footer update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { IntroPageComponent } from './intro-page/intro-page.component';
@@ -13,7 +14,8 @@ import { ExperienceComponent } from './experience/experience.component';
 @NgModule({
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   declarations: [
     AppComponent,
